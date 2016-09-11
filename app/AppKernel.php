2df009cd7220d45ff776e9300c0ca0ccf73e379a
Ivan Kalita <input.txt@hotmail.com>
2016-09-11 17:10:06 +1000

    Add fresh enum doctrine bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Hautelook\AliceBundle\HautelookAliceBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new Fresh\DoctrineEnumBundle\FreshDoctrineEnumBundle(),
 
             new AppBundle\AppBundle(),
         ];
