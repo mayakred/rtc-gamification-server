e467e5c47c175cd09a7125e765459df7d0828f48
Ivan Kalita <input.txt@hotmail.com>
2016-09-23 13:16:36 +1000

    Add entity managers

---
+++ src/AppBundle/Manager/DeviceManager.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ivan Kalita
+ * Date: 23.09.16
+ * Time: 12:01.
+ */
+namespace AppBundle\Manager;
+
+class DeviceManager
+{
+}
---
+++ src/AppBundle/Manager/PhoneManager.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ivan Kalita
+ * Date: 23.09.16
+ * Time: 12:00.
+ */
+namespace AppBundle\Manager;
+
+class PhoneManager extends BaseEntityManager
+{
+}
