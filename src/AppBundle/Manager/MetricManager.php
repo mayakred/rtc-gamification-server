0a24fb60dacbb3c0668d7f9b31c939b12cfa8c95
Ivan Kalita <input.txt@hotmail.com>
2016-09-24 13:09:37 +1000

    Add Duel methods

---
+++ src/AppBundle/Manager/MetricManager.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ivan Kalita
+ * Date: 24.09.16
+ * Time: 12:39.
+ */
+namespace AppBundle\Manager;
+
+class MetricManager extends BaseEntityManager
+{
+}
